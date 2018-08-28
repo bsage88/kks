ef3b5aaadd9b4698cc84203704f3678385864d8c
Brian Sage <bsage@brocksolutions.com>
2018-08-28 16:22:53 -0400

    more overlay fixes

--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import sign from '../images/sign.png';
-import merryChristmas from '../images/merryChristmas.jpg';
-import { toggleOverlay } from '../utils';
-import '../css/overlay.css';
+import React from "react";
+import sign from "../images/sign.png";
+import merryChristmas from "../images/merryChristmas.jpg";
+import { toggleOverlay } from "../utils";
+import "../css/overlay.css";
 
 export default function Overlay(props) {
-    return (
-        <div id="overlay" className="overlay" onClick={toggleOverlay}>
-            <img src={merryChristmas} alt="" />
-            <img className="sign" src={sign} alt="" />
-            <div className="sign-name">{props.name}</div>
-        </div>
-    );
+  return (
+    <div id="overlay" className="overlay" onClick={toggleOverlay}>
+      <img className="banner" src={merryChristmas} alt="" />
+      <div className="overlay-body">
+        <img className="sign" src={sign} alt="" />
+        <div className="sign-name">{props.name}</div>
+      </div>
+    </div>
+  );
 }
