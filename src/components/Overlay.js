9dd335ee7ec14f222e561a315f0b184c9fac2d6a
Brian Sage <bsage@brocksolutions.com>
2020-10-31 15:38:42 -0400

    - fixing some styles

--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import sign from '../images/sign.png';
 import merryChristmas from '../images/merryChristmas.jpg';
+import background from '../images/background.jpg';
 import { capitalizeFirstLetter } from '../utils';
 
 export default function Overlay(props) {
@@ -14,11 +15,14 @@ export default function Overlay(props) {
 
     return (
         <div id="overlay" className={className} onClick={props.hideOverlay}>
-            <img className="overlay__banner" src={merryChristmas} alt="" />
-            <div className="overlay__body">
-                <img className="overlay__sign" src={sign} alt="" />
-                <div className="overlay__sign-name">
-                    {capitalizeFirstLetter(props.name)}
+            <div className="overlay__shadow" />
+            <div className="overlay__background">
+                <img className="overlay__banner" src={merryChristmas} alt="" />
+                <div className="overlay__body">
+                    <img className="overlay__sign" src={sign} alt="" />
+                    <div className="overlay__sign-name">
+                        {capitalizeFirstLetter(props.name)}
+                    </div>
                 </div>
             </div>
         </div>
